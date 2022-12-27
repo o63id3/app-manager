b7f9e1833444abfb2f57813530f6f052dd2e3327
o63id3 <h.k.h.e.2000@gmail.com>
2022-12-27 11:22:28 +0200

    commit

--- test_main.py
+++ test_main.py
@@ -1,12 +1,14 @@
 import pytest
 import boto3
 
-import main
-from aws_keys import access_key_id, secret_access_key
-
 
 autoscaling_clinet = boto3.client('autoscaling', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, region_name='us-east-1')
 autoscaling_clinet.set_desired_capacity(AutoScalingGroupName='imagey_autoscaling_group', DesiredCapacity=7)
+
+
+import main
+from aws_keys import access_key_id, secret_access_key
+
 
 @pytest.fixture()
 def app():
@@ -44,52 +46,23 @@ def test_show_manager_page(client):
 
 # * Test inc success
 def test_inc_success(client):
-    # number of instances = 7
     response = client.post("/inc")
     assert response.status_code == 200
 
 # * Test inc failure
 def test_inc_failure(client):
-    # number of instances = 8
     response = client.post("/inc")
     assert response.status_code == 401
 
 
 # * Test dec success
 def test_dec_success(client):
-    # number of instances = 8
     response = client.post("/dec")
     assert response.status_code == 200
 
 # * Test dec failure
 def test_dec_failure(client):
-    # number of instances = 7
-    response = client.post("/dec")
-    assert response.status_code == 200
-    
-    # number of instances = 6
-    response = client.post("/dec")
-    assert response.status_code == 200
-    
-    # number of instances = 5
-    response = client.post("/dec")
-    assert response.status_code == 200
-    
-    # number of instances = 4
-    response = client.post("/dec")
-    assert response.status_code == 200
-    
-    # number of instances = 3
-    response = client.post("/dec")
-    assert response.status_code == 200
-    
-    # number of instances = 2
-    response = client.post("/dec")
-    assert response.status_code == 200
-    
-    # number of instances = 1
-    response = client.post("/dec")
-    assert response.status_code == 200
+    autoscaling_clinet.set_desired_capacity(AutoScalingGroupName='imagey_autoscaling_group', DesiredCapacity=0)
     
     response = client.post("/dec")
     assert response.status_code == 401
